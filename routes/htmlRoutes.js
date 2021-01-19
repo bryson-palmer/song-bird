c195708d8e8bc5a94100869f223c9225fce51d8f
Bryson Palmer <brysonpalmer@gmail.com>
2021-01-18 16:48:27 -0800

    Following the file structure of MVC, I moved the routes folder to the root level and changed relative paths accordingly.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,14 +3,14 @@ const path = require("path");
 const router = require("express").Router();
 
 // Requiring our custom middleware for checking if a user is logged in
-const isAuthenticated = require("../../config/middleware/isAuthenticated");
+const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 router.get("/", (req, res) => {
   // If the user already has an account send them to the members page
   if (req.user) {
     res.redirect("/members");
   }
-  res.sendFile(path.join(__dirname, "../signup.html"));
+  res.sendFile(path.join(__dirname, "../public/signup.html"));
 });
 
 router.get("/login", (req, res) => {
@@ -18,13 +18,13 @@ router.get("/login", (req, res) => {
   if (req.user) {
     res.redirect("/members");
   }
-  res.sendFile(path.join(__dirname, "../login.html"));
+  res.sendFile(path.join(__dirname, "../public/login.html"));
 });
 
 // Here we've add our isAuthenticated middleware to this route.
 // If a user who is not logged in tries to access this route they will be redirected to the signup page
 router.get("/members", isAuthenticated, (req, res) => {
-  res.sendFile(path.join(__dirname, "../members.html"));
+  res.sendFile(path.join(__dirname, "../public/members.html"));
 });
 
 // eslint-disable-next-line prettier/prettier
