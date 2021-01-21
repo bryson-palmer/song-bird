5534d2c5fc63df6c78781692903b063aed7f82f2
X.LI <ralflea@gmail.com>
2021-01-20 21:47:52 -0800

    nit

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
+-- Seeds for the database go here
 INSERT INTO  users (email, password) VALUES ('user@gmail.com','123');
 INSERT INTO songs (title, artist, songkey, tempo, UserId) VALUES ('Sugar', 'Maroon 5', 'Db Major','120', '1');
 INSERT INTO songs (title, artist, songkey, tempo, UserId) VALUES ('Girls like you', 'Maroon 5', 'D minor','96', '1');
