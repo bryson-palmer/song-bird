1076730f06c48e0799153a67d7a9b40ff50a3b2a
X.LI <ralflea@gmail.com>
2021-01-20 21:46:15 -0800

    add seed.sql and createAt, updateAt

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,5 @@
--- Seeds for the database go here
+INSERT INTO  users (email, password) VALUES ('user@gmail.com','123');
+INSERT INTO songs (title, artist, songkey, tempo, UserId) VALUES ('Sugar', 'Maroon 5', 'Db Major','120', '1');
+INSERT INTO songs (title, artist, songkey, tempo, UserId) VALUES ('Girls like you', 'Maroon 5', 'D minor','96', '1');
+INSERT INTO songs (title, artist, songkey, tempo, UserId) VALUES ('Shape of you', 'Ed Sheeran', 'C-sharp minor','96', '1');
+INSERT INTO songs (title, artist, songkey, tempo, UserId) VALUES ('Positions', 'Ariana Grande', 'D minor','72', '1');
