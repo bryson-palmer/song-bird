7f886d3a2f66b83b1b14b4768f92c1f3c54b16fb
Tskading <Tskading@gmail.com>
2021-01-25 20:20:54 -0800

    Delete functionality available via song page modal.

--- public/js/song.js
+++ public/js/song.js
@@ -30,3 +30,46 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 };
+
+///////////////////////// DELETE MODAL CODE BELOW //////////////////////////////////
+
+// Get the modal
+const delmodal = document.getElementById("delUpdate-modal");
+
+// Get the button that opens the modal
+const delbtn = document.getElementById("delUpdate-tab");
+
+// Get the <span> element that closes the modal
+const delspan = document.getElementsByClassName("delclose")[0];
+
+// del function
+const delSong = document.getElementById("delSong");
+
+delSong.onclick = function(e) {
+  e.preventDefault();
+  console.log(e.target.value);
+
+  $.ajax({
+    url: `/api/song/${e.target.value}`,
+    type: "DELETE"
+  }).then(() => {
+    window.location.href = "/members";
+  });
+};
+
+// When the user clicks on the button, open the modal
+delbtn.onclick = function() {
+  delmodal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+delspan.onclick = function() {
+  delmodal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target === delmodal) {
+    delmodal.style.display = "none";
+  }
+};
